2b57052bc10d8632b54b37139485cb390eb768d3
Yusuf <yusuftaha9@gmail.com>
2022-08-16 18:22:51 +0300

    Norm

--- src/bonus.c
+++ src/bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 18:17:37 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/08/16 18:19:27 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "bubble_sort.h"
 #include "sorters.h"
 #include "parse_args.h"
@@ -17,27 +29,27 @@
 
 void	execute_cmd(t_vars *vars, char *cmd)
 {
-	if (!ft_strncmp(cmd, "ss\n", 3))
+	if (!ft_strncmp(cmd, "ss\n", 5))
 		ss(vars);
-	else if (!ft_strncmp(cmd, "sa\n", 3))
+	else if (!ft_strncmp(cmd, "sa\n", 5))
 		sx(vars, vars->a);
-	else if (!ft_strncmp(cmd, "sb\n", 3))
+	else if (!ft_strncmp(cmd, "sb\n", 5))
 		sx(vars, vars->b);
-	else if (!ft_strncmp(cmd, "pa\n", 3))
-		px(vars, vars->a, vars->b);
-	else if (!ft_strncmp(cmd, "pb\n", 3))
+	else if (!ft_strncmp(cmd, "pa\n", 5))
 		px(vars, vars->b, vars->a);
-	else if (!ft_strncmp(cmd, "ra\n", 3))
+	else if (!ft_strncmp(cmd, "pb\n", 5))
+		px(vars, vars->a, vars->b);
+	else if (!ft_strncmp(cmd, "ra\n", 5))
 		rx(vars, vars->a);
-	else if (!ft_strncmp(cmd, "rb\n", 3))
+	else if (!ft_strncmp(cmd, "rb\n", 5))
 		rx(vars, vars->b);
-	else if (!ft_strncmp(cmd, "rr\n", 3))
+	else if (!ft_strncmp(cmd, "rr\n", 5))
 		rr(vars);
-	else if (!ft_strncmp(cmd, "rra\n", 3))
+	else if (!ft_strncmp(cmd, "rra\n", 5))
 		rrx(vars, vars->a);
-	else if (!ft_strncmp(cmd, "rrb\n", 3))
+	else if (!ft_strncmp(cmd, "rrb\n", 5))
 		rrx(vars, vars->b);
-	else if (!ft_strncmp(cmd, "rrr\n", 3))
+	else if (!ft_strncmp(cmd, "rrr\n", 5))
 		rrr(vars);
 	else
 		exit_program();
@@ -47,7 +59,7 @@ int	main(int argc, const char *argv[])
 {
 	t_vars	vars;
 	t_ivec	*parsed_args;
-	char		*cmd;
+	char	*cmd;
 
 	parsed_args = parse_args(argc, argv);
 	init(&vars, parsed_args);
